31b00d34cedb5b26f53d8aaeadf6b713bdb1570f
Joscha Feth <joscha@feth.com>
2014-07-17 00:52:42 +0200

    remove unused counter

--- test/e2e/SpecHelper.js
+++ test/e2e/SpecHelper.js
@@ -1,12 +1,10 @@
 /* global browser, global, protractor */
 var sandboxUrl = 'http://localhost:8765/test/e2e/env/';
-var i = 0;
 
 global.By = protractor.By;
 global.prot = protractor.getInstance();
 
 beforeEach(function() {
-  i = 0;
   browser.get(sandboxUrl);
 });
 
