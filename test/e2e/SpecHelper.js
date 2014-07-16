9c1dd07cdc9294651e621cb10740a0963e2e9421
Hannes Diercks <hi@xiphe.net>
2014-07-16 22:44:46 +0200

    use localhost again.

--- test/e2e/SpecHelper.js
+++ test/e2e/SpecHelper.js
@@ -1,5 +1,5 @@
 /* global browser, global, protractor */
-var sandboxUrl = 'http://127.0.0.1:8765/test/e2e/env/index.html';
+var sandboxUrl = 'http://localhost:8765/test/e2e/env/index.html';
 var i = 0;
 
 global.By = protractor.By;
