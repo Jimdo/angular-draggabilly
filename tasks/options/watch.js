14ef75ecb3a4735da4f7a3aa29df58568836234b
Hannes Diercks <github@xiphe.net>
2014-08-13 12:49:25 +0200

    remove traces of _test:beforeEach

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -8,14 +8,14 @@ var bothTestfiles = grunt.util._.clone(unitTestfiles).concat([files.e2eTests]);
 module.exports = {
   andtestunit: {
     files: unitTestfiles,
-    tasks: ['_test:beforeEach', 'karma:watch:run']
+    tasks: ['ngtemplates', 'karma:watch:run']
   },
   andteste2e: {
     files: e2eTestfiles,
-    tasks: ['_test:beforeEach', 'protractor:tdd']
+    tasks: ['ngtemplates', 'protractor:tdd']
   },
   andtestboth: {
     files: bothTestfiles,
-    tasks: ['_test:beforeEach', 'karma:watch:run', 'protractor:tdd']
+    tasks: ['ngtemplates', 'karma:watch:run', 'protractor:tdd']
   }
 };
