8ce1a4e1c4601b3236458d256de098cf8cbb8e72
Hauke Stange <hauke@jimdo.com>
2014-09-04 09:25:56 +0200

    fix #2 - remove wrapper directive in demo

--- demo/app.js
+++ demo/app.js
@@ -1,12 +1,1 @@
-angular.module('myApp', ['myModule']).directive('myApp', function() {
-  return {
-    restrict: 'E',
-    replace: false,
-    template: '<div my-directive></div>',
-    controller: ['$scope', function($scope) {
-      /* do something with $scope */
-      $scope.bar = 'lorem';
-    }]
-  };
-});
-angular.bootstrap(document, ['myApp']);
+angular.bootstrap(document, ['myModule']);
