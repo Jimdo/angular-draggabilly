536c248ea4fed6f646369b2907817233442f4758
Hannes Diercks <hi@xiphe.net>
2014-11-20 19:58:46 +0100

    style(directive): remove manual annotation from demo directive

--- src/js/directive.directive.js
+++ src/js/directive.directive.js
@@ -1,9 +1,9 @@
 /* global myModule */
-myModule.directive('myDirective', [function() {
+myModule.directive('myDirective', function() {
   return {
     templateUrl: 'directive.html',
-    controller: ['$scope', function($scope) {
+    controller: function($scope) {
       $scope.foo = 'bar';
-    }]
+    }
   };
-}]);
+});
