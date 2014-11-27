bf98e3b3deb67fd1358032d6ab95c927cb568d8a
Joscha Feth <joscha@feth.com>
2014-11-27 11:20:42 +0100

    Update SpecHelper.js

--- test/unit/SpecHelper.js
+++ test/unit/SpecHelper.js
@@ -6,6 +6,12 @@
 /* some globals we might need later on, set in beforeEach */
 var $rootScope, $compile, $injector, $httpBackend, $scope, $q, $controller;
 
+/**
+ * Initiate the angular module we want to test on and initiate
+ * global angular modules required for testing (like $rootScope etc.)
+ * 
+ * @param {bool} [withModule] disable automatic module initiation (by default, the module is initiated automatically for you)
+ */
 function initGlobals(withModule) {
   if (withModule !== false) {
     /* Initiate the main module */
